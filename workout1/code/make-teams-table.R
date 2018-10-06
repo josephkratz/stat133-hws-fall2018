c589285766024784e2c6d6e36ce9dcd008527038
Joey Kratz <josephkratz@berkeley.edu>
2018-10-05 20:10:22 -0700

    data processing

--- workout1/code/make-teams-table.R
+++ workout1/code/make-teams-table.R
@@ -8,3 +8,16 @@
 # Author: Joey Kratz
 # Date: 10-03-2018
 # ==================================================================
+library(readr)    # importing data
+library(dplyr)    # data wrangling
+dat <- read_csv("Desktop/stat133-hws-fall2018/workout1/data/nba2018.csv")
+dat$experience[dat$experience=="R"] = 0
+as.integer(dat$experience) #Replacing all "R"s with 0 and converting to integers
+dat$salary <- dat$salary/1000000 #making salary in terms of millions
+factor(dat$position, c("C", "PF", "PG", "SF", "SG"), 
+       c("center", "power_fwd", "point_guard", "small_fwd", "shoot_guard"))
+dat$position[dat$position=="C"] = "center"
+dat$position[dat$position=="PF"] = "power_fwd"
+dat$position[dat$position=="PG"] = "point_guard"
+dat$position[dat$position=="SF"] = "small_fwd"
+dat$position[dat$position=="SG"] = "shoot_guard"
