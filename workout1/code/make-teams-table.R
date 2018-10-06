a563b4da6aae551a82e8f99fb4717792a45df098
Joey Kratz <josephkratz@berkeley.edu>
2018-10-05 18:50:16 -0700

    creating R script

---
+++ workout1/code/make-teams-table.R
@@ -0,0 +1,10 @@
+# ==================================================================
+#Title: Workout 01
+# Description:
+#   This is a data preparation for the nba2018.csv. We will be
+#   Creating variables that will be used in the final analysis. 
+# Input(s): data file 'nba2018.csv'
+# Output(s): data file 'nba2018-teams.csv'
+# Author: Joey Kratz
+# Date: 10-03-2018
+# ==================================================================
