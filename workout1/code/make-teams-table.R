5e700e81c6aeffdc0f328483a2dcc456d5683a02
Joey Kratz <josephkratz@berkeley.edu>
2018-10-05 21:02:23 -0700

    finished R script

--- workout1/code/make-teams-table.R
+++ workout1/code/make-teams-table.R
@@ -30,6 +30,44 @@ dat %>% mutate(rebounds= off_rebounds+def_rebounds)
 
 dat %>% mutate(efficiency=points+rebounds + assists + steals + blocks - missed_fg - missed_ft - turnovers) / games_played
 
+sink(file="Desktop/stat133-hws-fall2018/workout1/output/efficiency-summary.txt")
+summary(dat$efficiency)
 sink()
+# Because I was in the /user directory, this is the path I used above. 
+# If I was in the /code directory, my path would have looked like
+# file = "../output/efficiency-summary.txt"
+dat$experience <- as.integer(dat$experience)
+team <- count(dat, team)[, 1]
+experience <- summarise(group_by(dat, team), experience = sum(experience))[, 2]
+salary <- summarise(group_by(dat, team), salary = round(sum(salary)), 2)[, 2]
+points3 <- summarise(group_by(dat, team), points3 = sum(points3))[, 2]
+points2 <- summarise(group_by(dat, team), points2 = sum(points2))[, 2]
+points1 <- summarise(group_by(dat, team), points1 = sum(points1))[, 2]
+points <-  summarise(group_by(dat, team), points = sum(points))[, 2]
+off_rebounds <- summarise(group_by(dat, team), off_rebounds = sum(off_rebounds))[, 2]
+def_rebounds <- summarise(group_by(dat, team), def_rebounds = sum(def_rebounds))[, 2]
+assists <- summarise(group_by(dat, team), assists = sum(assists))[, 2]
+steals <- summarise(group_by(dat, team), steals = sum(steals))[, 2]
+blocks <- summarise(group_by(dat, team), blocks = sum(blocks))[, 2]
+turnovers <- summarise(group_by(dat, team), turnovers = sum(turnovers))[, 2]
+fouls <- summarise(group_by(dat, team), fouls = sum(fouls))[, 2]
+efficiency <- summarise(group_by(dat, team), efficiency = sum(efficiency))[, 2]
+
+teams <- data.frame(team, experience, salary, points3, points2, points1, 
+                    points, off_rebounds, def_rebounds, assists, steals,
+                    blocks, turnovers, fouls, efficiency)
+
+sink(file="Desktop/stat133-hws-fall2018/workout1/output/teams-summary.txt")
+summary(teams)
+sink()
+
+write.csv(teams, file = "Desktop/stat133-hws-fall2018/workout1/data/nba2018-teams.csv", 
+          row.names = FALSE)
+
+
+
+
+
+
 
 
