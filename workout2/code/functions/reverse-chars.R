070e745986be8f64462928f061ee409b829f0cf6
Joey Kratz <josephkratz@berkeley.edu>
2018-11-04 22:16:36 -0800

    reverse char done

---
+++ workout2/code/functions/reverse-chars.R
@@ -0,0 +1,10 @@
+#' @title Reverse characters function function
+#' @description reverses characters in a string
+#' @param x string value (character)
+#' @return string value with characters reversed (character)
+
+reverse_chars <- function(x) {
+  split <- strplit(x, NULL)[[1]]
+  reversed <- rev(split)
+  return(paste(reversed, collapse = ''))
+}
