38c7d658a1e32dbc7bf5593fc2b36fabf45fc343
Joey Kratz <josephkratz@berkeley.edu>
2018-11-04 22:08:44 -0800

    hex done

--- workout2/code/functions/hex-color.R
+++ workout2/code/functions/hex-color.R
@@ -20,3 +20,29 @@ is_hex <- function(x) {
   }
 }
 
+#' @title Is this a valid hex color with alpha? function
+#' @description checks whether a value is a valid hex color with alpha
+#' @param x string value (character)
+#' @return boolean (TRUE/FALSE)
+
+is_hex_alpha <- function(x) {
+  if (!is.character(x)) {
+    stop("invalid input; a string was expected")
+  } else if (nchar(x) != 6 | nchar(x) != 8) {
+    return(FALSE)
+  } else {
+    x <- tolower(x)
+    if (substr(x, 1, 1) != "#") {
+      return(FALSE)
+    } else if (str_detect(x, "[0123456789ghijklmnopqrstuvwxyz]")) {
+      return(FALSE)
+    } else {
+      return(TRUE)
+    }
+  } 
+}
+
+
+
+
+
